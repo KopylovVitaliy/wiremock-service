e116a3cd6f3dd2d46a7c4ca0e1ef38eb86bc86b6
VitaliyK <v.kopylov@notamedia.com.ru>
2024-04-10 19:47:46 +0300

    Initial commit

---
+++ src/test/java/com/example/wiremockservice/WiremockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wiremockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
